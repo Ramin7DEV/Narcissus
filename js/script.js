383fcc26b835191f782d17db8168570faddab3f8
Ramin7DEV <raminbakhshaliyev@gmail.com>
2025-12-26 18:30:48 +0400

    Initial commit: Narcissus Flower Animation with Van Gogh Starry Night background

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Narcissus Flower Animation
+ * Initializes the blooming animation after page load
+ */
+
+window.onload = function() {
+  const timeout = setTimeout(function() {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(timeout);
+  }, 1000);
+};
