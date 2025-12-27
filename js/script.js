9f0244bf6904b6d343aacdb7f69392362233a579
Ramin7DEV <raminbakhshaliyev@gmail.com>
2025-12-27 17:33:58 +0400

    feat: Add animated fireflies, magic particles, and butterflies, and adjust flower initial position.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
  * Initializes the blooming animation after page load
  */
 
-window.onload = function() {
-  const timeout = setTimeout(function() {
+window.onload = function () {
+  const timeout = setTimeout(function () {
     document.body.classList.remove("not-loaded");
     clearTimeout(timeout);
   }, 1000);
